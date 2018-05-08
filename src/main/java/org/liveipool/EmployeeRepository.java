52ca2013c4c3f905d401bb36e8ff021ded10d08a
Liveipool <123653079@qq.com>
2018-05-08 17:39:47 +0800

    删除掉例子employee

--- src/main/java/org/liveipool/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package org.liveipool;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-/**
- * Created by liveipool on 2018/5/6.
- */
-public interface EmployeeRepository extends JpaRepository<Employee,String> {
-}
